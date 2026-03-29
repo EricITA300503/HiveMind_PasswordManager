d8975034041488cba8b37e68de8ad00380f66fbb
Miguel Lumaban <miglumaban@gmail.com>
2026-03-29 17:11:07 -0400

    added models and dbcontext

---
+++ PasswordManager/PasswordManager/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PasswordManager.Models;
+
+namespace PasswordManager.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<VaultEntry> VaultEntries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<VaultEntry>()
+                .HasOne(v => v.User)
+                .WithMany()
+                .HasForeignKey(v => v._userId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Category>()
+                .HasOne(c => c.User)
+                .WithMany()
+                .HasForeignKey(c => c._userId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ActivityLog>()
+                .HasOne(a => a.User)
+                .WithMany()
+                .HasForeignKey(a => a._userId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
