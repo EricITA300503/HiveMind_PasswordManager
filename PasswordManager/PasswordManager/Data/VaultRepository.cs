2657a8679adcd24789a8fc3414efffce22e99ac7
henrimau <henrimau@sheridancollege.ca>
2026-03-29 21:00:51 -0400

    Section 3

---
+++ PasswordManager/PasswordManager/Data/VaultRepository.cs
@@ -0,0 +1,40 @@
+﻿using PasswordManager.Models;
+
+namespace PasswordManager.Data
+{
+
+
+    //EF core implementation
+
+    public class VaultRepository : IVaultRepository
+    {
+        private readonly AppDbContext _context;
+
+
+
+        public VaultRepository(AppDbContext context) => _context = context;
+
+        public IQueryable<VaultEntry> GetAll(string userId) =>
+            _context.VaultEntries.Where(v => v._userId == userId);
+
+        public VaultEntry? GetById(int id) =>
+            _context.VaultEntries.FirstOrDefault(v => v._entryId == id);
+
+        public void Add(VaultEntry entry) => _context.VaultEntries.Add(entry);
+
+
+        public void Update(VaultEntry entry) => _context.VaultEntries.Update(entry);
+
+
+        public void Delete(int id)
+        {
+            var entry = GetById(id);
+
+
+
+            if (entry != null) _context.VaultEntries.Remove(entry);
+        }
+
+        //Save
+        public void Save() => _context.SaveChanges();
+    }
--- PasswordManager/PasswordManager/Program.cs
+++ PasswordManager/PasswordManager/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using PasswordManager.Data;
 using PasswordManager.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Here I'm going to register DbContext
 builder.Services.AddDbContext<AppDbContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IVaultRepository, VaultRepository>();
 
 //Here I'm just gonna register the identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
@@ -15,6 +17,9 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 //Now I'm registering MVC   
 builder.Services.AddControllersWithViews();
+
+
+
 
 var app = builder.Build();
 
