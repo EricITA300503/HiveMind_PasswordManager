d4ada4e5fe6538094875c5f477ea3067cabaaa52
EricITA300503 <eric.bertero03@gmail.com>
2026-03-29 21:22:51 -0400

    Part 5 Again

--- PasswordManager/PasswordManager/obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
+++ PasswordManager/PasswordManager/obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610a26efc1098f9bd44954e97453fcde7b78f9c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04272e697926b3c2a133ac93d5ec6fa3bb53177")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
