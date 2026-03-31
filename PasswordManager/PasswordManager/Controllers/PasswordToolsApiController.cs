44c786d0b4174b09fbc9c8c8f044448b1a744277
henrimau <henrimau@sheridancollege.ca>
2026-03-30 21:09:00 -0400

    Section 6.1

---
+++ PasswordManager/PasswordManager/Controllers/PasswordToolsApiController.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Models.ViewModels;
+using PasswordManager.Services;
+
+namespace PasswordManager.ApiControllers
+{
+
+    [ApiController]
+    [Route("api/passwordtools")]
+
+
+
+    public class PasswordToolsApiController : ControllerBase
+    {
+        private readonly IPasswordService _passwordService;
+
+        public PasswordToolsApiController(IPasswordService passwordService)
+            => _passwordService = passwordService;
+
+
+
+       
+        [HttpPost("generate")]
+        public IActionResult Generate([FromBody] PasswordOptions opts)
+        {
+            var password = _passwordService.GeneratePassword(opts);
+
+
+            return Ok(new { password });
+        }
+
+        
+
+
+        [HttpGet("strength")]
+        public IActionResult CheckStrength([FromQuery] string pw)
+        {
+            var result = _passwordService.CheckStrength(pw);
+
+
+            return Ok(result);
+
+
+
+        }
+    }
+
